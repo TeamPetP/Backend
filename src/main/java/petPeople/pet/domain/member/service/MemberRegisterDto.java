b60e1d4ab11fec8d8fde230f68b45a6da00415da
issiscv <issiscv46@gmail.com>
2022-05-08 21:08:34 +0900

    FEAT: 파이어베이스 회원 가입, 로그인 API 개발

--- src/main/java/petPeople/pet/domain/member/service/MemberRegisterDto.java
+++ src/main/java/petPeople/pet/domain/member/service/MemberRegisterDto.java
@@ -1,9 +1,11 @@
 package petPeople.pet.domain.member.service;
 
+import com.google.firebase.auth.FirebaseToken;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import petPeople.pet.controller.member.dto.req.MemberLocalRegisterRequestDto;
+import petPeople.pet.controller.member.dto.req.MemberRegisterRequestDto;
 
 @AllArgsConstructor
 @Getter @Setter
@@ -29,5 +31,14 @@ public class MemberRegisterDto {
         this.nickname = memberLocalRegisterRequestDto.getNickname();
         this.imgUrl = memberLocalRegisterRequestDto.getImgUrl();
         this.introduce = memberLocalRegisterRequestDto.getIntroduce();
+    }
+
+    public MemberRegisterDto(FirebaseToken firebaseToken, MemberRegisterRequestDto memberRegisterRequestDto) {
+        this.uid = firebaseToken.getUid();
+        this.name = firebaseToken.getName();
+        this.email = firebaseToken.getEmail();
+        this.nickname = memberRegisterRequestDto.getNickname();
+        this.imgUrl = firebaseToken.getPicture();
+        this.introduce = memberRegisterRequestDto.getIntroduce();
     }
 }
--- src/main/java/petPeople/pet/domain/member/service/MemberService.java
+++ src/main/java/petPeople/pet/domain/member/service/MemberService.java
@@ -6,27 +6,46 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import petPeople.pet.controller.member.dto.resp.MemberRegisterResponseDto;
 import petPeople.pet.domain.member.entity.Member;
 import petPeople.pet.domain.member.repository.MemberRepository;
+import petPeople.pet.exception.CustomException;
+import petPeople.pet.exception.ErrorCode;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService implements UserDetailsService {
+
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String uid) throws UsernameNotFoundException {
-        return memberRepository.findByUid(uid)
-                .orElseThrow(() -> {
+    public UserDetails loadUserByUsername(final String uid) throws UsernameNotFoundException {
+        return findOptionalMember(uid).orElseThrow(() -> {
                     throw new UsernameNotFoundException("해당 회원이 존재하지 않습니다.");
                 });
     }
 
     @Transactional
-    public void save(final MemberRegisterDto memberRegisterDto) {
-        Member member = createMember(memberRegisterDto);
-        memberRepository.save(member);
+    public MemberRegisterResponseDto register(MemberRegisterDto memberRegisterDto) {
+        validateDuplicatedMember(memberRegisterDto.getUid());
+        return new MemberRegisterResponseDto(saveMember(memberRegisterDto));
+    }
+
+    private Member saveMember(MemberRegisterDto memberRegisterDto) {
+        return memberRepository.save(createMember(memberRegisterDto));
+    }
+
+    private void validateDuplicatedMember(String uid) {
+        if (findOptionalMember(uid).isPresent()) {
+            throw new CustomException(ErrorCode.EXIST_MEMBER, "해당 계정으로 이미 회원가입을 했습니다.");
+        }
+    }
+
+    private Optional<Member> findOptionalMember(String uid) {
+        return memberRepository.findByUid(uid);
     }
 
     private Member createMember(MemberRegisterDto memberRegisterDto) {
