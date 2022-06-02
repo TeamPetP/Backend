6060f7fd870919a91d8508bb2bde56dbf9261b3e
Longstick0 <789456jang@naver.com>
2022-06-02 16:10:45 +0900

    CICD: nohup 실행 시 CodeDeploy는 무한 대기 이를 해결하기 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용함 사용하지 않으면 nohup.out 파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력된다. nohup이 끝나기 전까지 CodeDeploy도 끝나지 않으니 꼭 해야 한다.

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ fi
 echo "> $JAR_PATH 배포"
 s
 #23412
-nohup java -jar $JAR_PATH --spring.profiles.active=prod /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH --spring.profiles.active=prod > $REPOSITORY/nohup.out 2>&1 &
