0c32284567ee4c13a1810aa3e743c678d0b52502
Longstick0 <789456jang@naver.com>
2022-06-02 15:31:34 +0900

    CICD: delpoy.sh 파일 오타 수정 camping -> pet

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/Backend
 cd $REPOSITORY
 
-APP_NAME=camping
+APP_NAME=pet
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
