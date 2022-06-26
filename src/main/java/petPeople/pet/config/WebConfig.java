064ad3049abb83a7c53023a442ce754dda011470
issiscv <issiscv46@gmail.com>
2022-06-26 16:55:08 +0900

    BUILD: 배포를 위한 빌드

--- src/main/java/petPeople/pet/config/WebConfig.java
+++ src/main/java/petPeople/pet/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
                 .allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
                 .allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST...)
-                .allowCredentials(false) // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
+                .allowCredentials(true) // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
                 .maxAge(1500); // preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간 ;
     }
 
