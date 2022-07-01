926a10c2e627b28f9574ec61d73a3865a9f65925
Longstick0 <789456jang@naver.com>
2022-07-01 17:32:46 +0900

    DOCS : sawgger 문서 작성

--- src/main/java/petPeople/pet/controller/comment/dto/resp/CommentWriteRespDto.java
+++ src/main/java/petPeople/pet/controller/comment/dto/resp/CommentWriteRespDto.java
@@ -1,5 +1,7 @@
 package petPeople.pet.controller.comment.dto.resp;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -11,16 +13,22 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "댓글 작성 응답 DTO")
 public class CommentWriteRespDto {
 
+    @ApiModelProperty(required = true, value = "댓글 ID", example = "1")
     private Long commentId;
 
+    @ApiModelProperty(required = true, value = "회원 ID", example = "2")
     private Long memberId;
 
+    @ApiModelProperty(required = true, value = "게시글 ID", example = "2")
     private Long postId;
 
+    @ApiModelProperty(required = true, value = "댓글 내용", example = "물고기 산책 시키러 가실 분!")
     private String content;
 
+    @ApiModelProperty(required = true, value = "작성 시간", example = "2022-06-28T07:38:14.152321")
     private LocalDateTime createDate;
 
     public CommentWriteRespDto(Comment comment) {
