156d51c3712ba14e33d20a72b805cdee00093b3d
Longstick0 <789456jang@naver.com>
2022-07-24 17:15:46 +0900

    FIX: 알림 전체, 단건 삭제 시 알림 리스트 반환

--- src/main/java/petPeople/pet/controller/notification/NotificationController.java
+++ src/main/java/petPeople/pet/controller/notification/NotificationController.java
@@ -3,12 +3,16 @@ package petPeople.pet.controller.notification;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import petPeople.pet.controller.member.dto.resp.notificationResp.MemberNotificationResponseDto;
 import petPeople.pet.domain.member.entity.Member;
 import petPeople.pet.domain.notification.entity.Notification;
 import petPeople.pet.domain.notification.service.NotificationService;
@@ -23,9 +27,12 @@ public class NotificationController {
     @ApiOperation(value = "회원 알림 단건 삭제 API", notes = "삭제할 알림의 notificationId를 경로변수에 넣어주세요. 알림 조회를 위해 header에 토큰을 입력해주세요")
     @DeleteMapping("{notificationId}")
     public ResponseEntity deleteNotification(@ApiParam(value = "알림 ID", required = true) @PathVariable Long notificationId,
+                                             Pageable pageable,
                                              Authentication authentication) {
+
         Member member = (Member) authentication.getPrincipal();
         notificationService.deleteNotification(notificationId, member);
-        return ResponseEntity.noContent().build();
+        Slice<MemberNotificationResponseDto> memberNotificationResponseDtos = notificationService.retrieveNotifications(member.getId(), pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(memberNotificationResponseDtos);
     }
 }
