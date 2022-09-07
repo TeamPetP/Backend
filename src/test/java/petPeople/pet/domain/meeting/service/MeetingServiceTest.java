6909a78f52cc6d1a1bad7f0f7c4dc7139698e316
issiscv <issiscv46@gmail.com>
2022-09-07 17:04:35 +0900

    TEST: meetingPostService 서비스 계층 단위테스트 코드 작성

--- src/test/java/petPeople/pet/domain/meeting/service/MeetingServiceTest.java
+++ src/test/java/petPeople/pet/domain/meeting/service/MeetingServiceTest.java
@@ -33,7 +33,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
