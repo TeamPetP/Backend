4e8c6733ed4ecdbf09f447c5b23c51785bbc83ec
issiscv <issiscv46@gmail.com>
2022-09-20 15:26:46 +0900

    Revert "ETC: firebase SDK json 파일 환경변수 세팅"
    This reverts commit 0dd07d7f21a9cbdae254ca489b748e91d35279e5.

--- src/main/java/petPeople/pet/config/FirebaseConfig.java
+++ src/main/java/petPeople/pet/config/FirebaseConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -17,16 +16,19 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
-        String getenv = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
-
-        FileInputStream serviceAccount = new FileInputStream(getenv);
-
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(getFirebaseIs()))
                 .build();
 
         FirebaseApp.initializeApp(options);
         return FirebaseAuth.getInstance(FirebaseApp.getInstance());
     }
 
+    private InputStream getFirebaseIs() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("serviceAccountKey.json");
+        if(resource.exists()) {
+            return resource.getInputStream();
+        } throw new RuntimeException("firebase 키가 존재하지 않습니다");
+    }
 }
